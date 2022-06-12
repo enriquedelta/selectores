43d7b60de8b9747c187771819db95d44955b0c25
Enrique Morales <dtsdeltasoft@gmail.com>
2022-06-12 18:50:22 +0200

    llenado combo paises

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -9,12 +11,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required ]
+    region: ['', Validators.required ],
+    pais: ['', Validators.required ],
   });
 
-  constructor( private fb: FormBuilder ) { }
+  // llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+
+  constructor( private fb: FormBuilder,
+               private paisesService: PaisesService ) { }
 
   ngOnInit(): void {
+
+    this.regiones = this.paisesService.regiones;
+
+    // Cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe( region => {
+        console.log( region );
+        this.paisesService.getPaisesPorRegion( region )
+          .subscribe( paises => {
+            console.log(paises);
+            this.paises = paises;
+          })
+      });
+
   }
 
   guardar() {
