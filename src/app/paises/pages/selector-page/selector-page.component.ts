02777d7d9dcfde439690f7e7b4c04cfa93b99114
Enrique Morales <dtsdeltasoft@gmail.com>
2022-06-12 18:13:20 +0200

    Segundo commit

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styleUrls: ['./selector-page.component.css']
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required ]
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log( this.miFormulario.value );
+  }
+
+}
